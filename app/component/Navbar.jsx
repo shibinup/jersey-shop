046f36e140262302adb24ca674e3073ad72a5a47
shibin <upshiibin90@gmail.com>
2026-04-15 18:18:24 +0530

    profile page updated

--- app/component/Navbar.jsx
+++ app/component/Navbar.jsx
@@ -54,11 +54,16 @@ export default function Navbar() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
               </svg>
             </button>
+
+           {/*button for navigate in to profile*/}
+          <Link href={"/profile"}>
             <button className="hidden sm:block text-gray-900 hover:text-blue-600 transition-colors">
               <svg className="h-[22px] w-[22px]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
               </svg>
             </button>
+          
+          </Link>
 
             {/* button for add to cart icon*/}
             <Link href={"/cart"}>
