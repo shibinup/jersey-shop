561fa469d5017d9e6b419483e451552adfb15922
shibin <upshiibin90@gmail.com>
2026-05-10 17:53:25 +0530

    made params

--- app/component/Pricefilter.jsx
+++ app/component/Pricefilter.jsx
@@ -1,13 +1,32 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from 'next/navigation'
+import { useState ,useEffect ,useRef} from "react";
+import { useRouter,useSearchParams } from 'next/navigation'
 export default function Filters() {
+
+
   const [price, setPrice] = useState(400);
   const [selectedColor, setSelectedColor] = useState(null);
 
   const colors = ["Blue", "White", "Yellow", "Black"];
   const router=useRouter()
+  const searchParams = useSearchParams();
+  const mounted = useRef(false)
+   useEffect(() => {
+  if (!mounted.current) {
+    mounted.current = true;
+    return;
+  }
+
+  
+  const params = new URLSearchParams(searchParams.toString());
+  params.set("price", `${price}`)
+  params.set("color",`${selectedColor}`);
+  router.replace(`?${params.toString()}`);
+
+}, [price, selectedColor]);
+
+
 
   return (
     <div className="w-full max-w-sm bg-white p-5 rounded-2xl shadow-md space-y-6">
@@ -21,6 +40,7 @@ export default function Filters() {
           min="200"
           max="400"
           step="50"
+        
           value={price}
           onChange={(e) => setPrice(Number(e.target.value))}
           className="w-full"
@@ -46,6 +66,7 @@ export default function Filters() {
               key={color}
               onClick={() =>
                 setSelectedColor(selectedColor === color ? null : color)
+              
               }
               className={`px-4 py-2 rounded-xl border font-medium transition
                 ${
